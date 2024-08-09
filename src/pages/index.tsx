d3bbb7fc43af095cb8f3958652a1c3508d694814
fiimaarif <fii.ma'arif@baf.id>
2024-08-09 14:09:05 +0700

    [UPDATE]-Update url meta image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <meta name="description" content="I am fii maarif a frontend developer, I have experience in creating and developing responsive and intuitive UI." />
       <meta property="og:title" content="fii maarif personal website"/>
       <meta property="og:description" content="I am fii maarif a frontend developer, I have experience in creating and developing responsive and intuitive UI."/>
-      {/* <meta property="og:image" content="https://fiimaarif-iobjs3orp-fiimaarif.vercel.app/favicon.png"/> */}
+      <meta property="og:image" content="https://aboutfi.vercel.app/favicon.png"/>
       <meta property="og:url" content="https://aboutfi.vercel.app"/>
     </Head>
     <div className="flex flex-col md:flex-row md:p-10 font-roboto">
