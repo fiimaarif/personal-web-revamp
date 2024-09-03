f5aeca0d49b17931860c52537702eb191195c26a
fiimaarif <fii.ma'arif@baf.id>
2024-09-03 16:59:12 +0700

    [UPDATE]-Apply dark mode style in resume component

--- src/components/main/resume/index.tsx
+++ src/components/main/resume/index.tsx
@@ -6,22 +6,22 @@ import knowledges from "../../utils/knowledges.json";
 
 function Resume() {
     return (
-        <main className="p-8 bg-slate-50 rounded-3xl">
-            <h1 className="text-2xl font-semibold text-gray-800 mb-5">Resume</h1>
+        <main className="p-8 bg-slate-50 dark:bg-slate-800 rounded-3xl">
+            <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50 mb-5">Resume</h1>
             <div className="md:flex block gap-5">
                 <div className="w-full md:w-1/2">
                     <div className="flex items-start mb-7">
                         <Grid size={30} color="#0891b2" />
-                        <p className="ml-1 mt-1 text-xl">Education</p>
+                        <p className="ml-1 mt-1 text-xl dark:text-gray-50">Education</p>
                     </div>
                     {education.map((item) => (
-                        <div className="bg-red-50 p-5 rounded-2xl mb-5">
+                        <div className="bg-red-50 dark:bg-slate-700 p-5 rounded-2xl mb-5">
                         <div className="flex justify-between mb-2">
-                            <p className="text-sm text-gray-600">{item.year}</p>
-                            <p className="text-sm text-gray-600">{item.degree}</p>
+                            <p className="text-sm text-gray-600 dark:text-gray-50">{item.year}</p>
+                            <p className="text-sm text-gray-600 dark:text-gray-50">{item.degree}</p>
                         </div>
-                        <p className="text-xl mb-1">{item.university}</p>
-                        <p className="text-base text-gray-600">
+                        <p className="text-xl mb-1 dark:text-gray-50">{item.university}</p>
+                        <p className="text-base text-gray-600 dark:text-gray-50">
                             {item.major}
                         </p>
                     </div>
@@ -30,24 +30,24 @@ function Resume() {
                 <div className="w-full md:w-1/2">
                     <div className="flex items-start mb-7 mt-7 md:mt-0">
                         <Briefcase size={30} color="#0891b2" />
-                        <p className="ml-1 mt-1 text-xl">Experience</p>
+                        <p className="ml-1 mt-1 text-xl dark:text-gray-50">Experience</p>
                     </div>
                     {experience.map((item) => (
-                        <div className="bg-emerald-50 p-5 rounded-2xl mb-5">
+                        <div className="bg-emerald-50 dark:bg-slate-700 p-5 rounded-2xl mb-5">
                             <div className="flex justify-between mb-2">
-                                <p className="text-sm text-gray-600">
+                                <p className="text-sm text-gray-600 dark:text-gray-50">
                                     {item.year}
                                 </p>
                             </div>
-                            <p className="text-xl mb-1">{item.role}</p>
-                            <p className="text-base text-gray-600">
+                            <p className="text-xl mb-1 dark:text-gray-50">{item.role}</p>
+                            <p className="text-base text-gray-600 dark:text-gray-50">
                                 {item.office}
                             </p>
                         </div>
                     ))}
                 </div>
             </div>
-            <h1 className="text-2xl font-semibold text-gray-800 my-5">Knowledges</h1>
+            <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50 my-5">Knowledges</h1>
             <div className="flex gap-1 md:gap-1 lg:gap-x-4 lg:gap-y-3 flex-wrap justify-start">
                 {knowledges.map((item, index) => (
                     <div key={index} className="bg-red-400 px-5 py-1 rounded-3xl font-medium text-gray-50">{item.label}</div>
