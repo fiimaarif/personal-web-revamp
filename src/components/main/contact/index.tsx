c45160932a31be686dd8881ff32fde88dc737e90
fiimaarif <fii.ma'arif@baf.id>
2024-09-03 17:06:37 +0700

    [UPDATE]-Apply dark mode style in contact component

--- src/components/main/contact/index.tsx
+++ src/components/main/contact/index.tsx
@@ -4,16 +4,16 @@ import { Send } from "react-feather";
 
 function Contact() {
     return (
-        <main className="p-8 bg-slate-50 rounded-3xl">
-            <h1 className="text-2xl font-semibold text-gray-800 mb-5">
+        <main className="p-8 bg-slate-50 dark:bg-slate-800 rounded-3xl">
+            <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50 mb-5">
                 Contact
             </h1>
             <article className="py-5 w-full md:w-full xl:w-8/12">
-                <p className="text-base md:text-lg text-neutral-600">I appreciate your interest in reaching out to me.</p>
-                <p className="text-base md:text-lg text-neutral-600">I'll do my best to respond promptly.</p>
-                <p className="text-base md:text-lg text-neutral-600">Thank you for your attention.</p>
+                <p className="text-base md:text-lg text-neutral-600 dark:text-gray-50">I appreciate your interest in reaching out to me.</p>
+                <p className="text-base md:text-lg text-neutral-600 dark:text-gray-50">I'll do my best to respond promptly.</p>
+                <p className="text-base md:text-lg text-neutral-600 dark:text-gray-50">Thank you for your attention.</p>
             </article>
-            <div className="bg-slate-100 p-10 rounded-3xl">
+            <div className="bg-slate-100 dark:bg-slate-700 p-10 rounded-3xl">
                 <CustomForm type="text" label="name" />
                 <CustomForm type="email" label="email" />
                 <CustomForm type="textarea" label="message" />
