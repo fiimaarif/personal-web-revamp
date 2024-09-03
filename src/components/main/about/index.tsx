4bc0682a8bcc2fd3eac8f63190b3df3c694fce4b
fiimaarif <fii.ma'arif@baf.id>
2024-09-03 16:54:29 +0700

    [UPDATE]-Apply dark mode style in about component

--- src/components/main/about/index.tsx
+++ src/components/main/about/index.tsx
@@ -4,12 +4,12 @@ import { Codesandbox, Layout } from "react-feather";
 
 function About() {
     return (
-        <main className="p-8 bg-slate-50 rounded-3xl">
-            <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 mb-5">
+        <main className="p-8 bg-slate-50 dark:bg-slate-800 rounded-3xl">
+            <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 dark:text-gray-50 mb-5">
                 About
             </h1>
             <article>
-                <p className="text-base xl:text-xl text-justify text-neutral-600">
+                <p className="text-base xl:text-xl text-justify text-neutral-600 dark:text-gray-50">
                     Hi there, I'm fii maarif. I am a Frontend Developer. I have
                     experience in creating and developing responsive and
                     intuitive user interfaces for web applications. I possess
@@ -17,7 +17,7 @@ function About() {
                     using frameworks such as ReactJS, Angular and React Native
                     for Mobile.
                 </p>
-                <p className="mt-5 text-base xl:text-xl text-justify text-neutral-600">
+                <p className="mt-5 text-base xl:text-xl text-justify text-neutral-600 dark:text-gray-50">
                     I am also able to work closely with the backend team to
                     ensure seamless integration between the user interface and
                     systems. I am passionate about learning and experimenting
@@ -25,23 +25,23 @@ function About() {
                 </p>
             </article>
             <div>
-                <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 my-5">
+                <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 dark:text-gray-50 my-5">
                     What I Do!
                 </h1>
                 <div className="grid grid-cols-12 gap-5">
-                    <div className="bg-red-50 p-3 col-span-12 md:col-span-6 rounded-xl">
-                        <div className="bg-emerald-100 w-min p-2 rounded-xl mb-2">
+                    <div className="bg-red-50 dark:bg-slate-700 p-3 col-span-12 md:col-span-6 rounded-xl">
+                        <div className="bg-emerald-100 dark:bg-slate-500 w-min p-2 rounded-xl mb-2">
                             <Layout
                                 size={30}
                                 color="#d946ef"
                             />
                         </div>
                             <div>
-                                <p className="text-xl text-neutral-800 font-semibold">
+                                <p className="text-xl text-neutral-800 dark:text-gray-50 font-semibold">
                                     Web Development
                                 </p>
                                 <hr className="my-1"/>
-                                <p className="text-neutral-600 text-left">
+                                <p className="text-neutral-600 dark:text-gray-50 text-left">
                                     I develop modern, responsive websites using
                                     the latest technologies to ensure optimal
                                     performance and an outstanding user
@@ -49,19 +49,19 @@ function About() {
                                 </p>
                             </div>
                     </div>
-                    <div className="bg-emerald-50 p-3 col-span-12 md:col-span-6 rounded-xl">
-                            <div className="bg-red-100 w-min p-2 rounded-xl mb-2">
+                    <div className="bg-emerald-50 dark:bg-slate-700 p-3 col-span-12 md:col-span-6 rounded-xl">
+                            <div className="bg-red-100 dark:bg-slate-500 w-min p-2 rounded-xl mb-2">
                                 <Codesandbox
                                     size={30}
                                     color="#fb7185"
                                 />
                             </div>
                             <div>
-                                <p className="text-xl text-neutral-800 font-semibold">
+                                <p className="text-xl text-neutral-800 dark:text-gray-50 font-semibold">
                                     Mobile Development
                                 </p>
                                 <hr className="my-1"/>
-                                <p className="text-neutral-600 text-left">
+                                <p className="text-neutral-600 dark:text-gray-50 text-left">
                                     I create high-performance mobile
                                     applications tailored to your needs,
                                     ensuring seamless functionality and an
@@ -72,11 +72,11 @@ function About() {
                 </div>
             </div>
             <div>
-                <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 my-5">
+                <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 dark:text-gray-50 my-5">
                     Languages and Tools
                 </h1>
                 <div>
-                    <div className="flex flex-wrap justify-center gap-5 mt-10 bg-gray-100 p-7 rounded-2xl">
+                    <div className="flex flex-wrap justify-center gap-5 mt-10 bg-gray-100 dark:bg-slate-700 p-7 rounded-2xl">
                         {tech.map((item, index) => (
                             <img
                                 key={index}
