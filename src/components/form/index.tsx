e5842b52433384777229ebf35d66b93568383e81
fiimaarif <fiimaarif44@gmail.com>
2025-04-10 14:50:19 +0700

    [UPDATE] - add resize-none class to textarea for better mobile responsiveness

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -10,7 +10,7 @@ function CustomForm({label, type, name} : CustomFormProps) {
     return (
         <div className="custom-form-group">
             {type === 'textarea' ? (
-                <textarea name={name} className="custom-form-input custom-form-textarea dark:text-gray-50" required />
+                <textarea name={name} className="custom-form-input custom-form-textarea resize-none dark:text-gray-50" required />
             ) : (
                 <input name={name} type={type} className="custom-form-input dark:text-gray-50" required />
             )}
