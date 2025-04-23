d76d3fb910e85abf9349f95390d79e937dbd696f
fiimaarif <fiimaarif44@gmail.com>
2025-04-23 21:09:23 +0700

    [UPDATE] - format contact section for improved readability and maintainability

--- src/components/main/contact/index.tsx
+++ src/components/main/contact/index.tsx
@@ -10,14 +10,30 @@ function Contact() {
                 Contact
             </h1>
             <article className="py-5 w-full md:w-full xl:w-8/12">
-                <p className="text-base md:text-lg text-neutral-600 dark:text-gray-50 text-justify md:text-left">I'm excited to connect with you! Whether you have questions, want to discuss a project, or just want to say hello, feel free to get in touch. You can reach me via email, phone, or through my social media profiles. I'll respond as soon as I can.</p>
+                <p className="text-base md:text-lg text-neutral-600 dark:text-gray-50 text-justify md:text-left">
+                    I'm excited to connect with you! Whether you have questions,
+                    want to discuss a project, or just want to say hello, feel
+                    free to get in touch. You can reach me via email, phone, or
+                    through my social media profiles. I'll respond as soon as I
+                    can.
+                </p>
             </article>
             <div className="bg-slate-100 dark:bg-slate-700 p-10 rounded-3xl">
-                <form action={`https://formspree.io/f/${formspreeId}`} method="POST">
+                <form
+                    action={`https://formspree.io/f/${formspreeId}`}
+                    method="POST"
+                >
                     <CustomForm type="text" label="name" name="name" />
                     <CustomForm type="email" label="email" name="email" />
-                    <CustomForm type="textarea" label="message" name="message" />
-                    <button type="submit" className="mt-4 flex bg-indigo-500 text-white py-3 px-6 rounded-xl hover:bg-indigo-600 transition duration-300">
+                    <CustomForm
+                        type="textarea"
+                        label="message"
+                        name="message"
+                    />
+                    <button
+                        type="submit"
+                        className="mt-4 flex bg-indigo-500 text-white py-3 px-6 rounded-xl hover:bg-indigo-600 transition duration-300"
+                    >
                         <Send size={19} className="mr-1" />
                         Submit
                     </button>
