4eee9b57616d97933934f2ea03276ce2549c9661
fiimaarif <fiimaarif44@gmail.com>
2025-05-13 21:45:09 +0700

    [UPDATE] - adjust layout in Resume component for improved responsiveness

--- src/components/main/resume/index.tsx
+++ src/components/main/resume/index.tsx
@@ -8,8 +8,8 @@ function Resume() {
     return (
         <main className="p-8 bg-slate-50 dark:bg-slate-800 rounded-3xl mt-2 md:mt-0 mb-10 md:mb-0">
             <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50 mb-5">Resume</h1>
-            <div className="md:flex block gap-5">
-                <div className="w-full md:w-1/2">
+            <div className="lg:flex block gap-5">
+                <div className="w-full lg:w-1/2">
                     <div className="flex items-start mb-7">
                         <Grid size={30} color="#0891b2" />
                         <p className="ml-1 mt-1 text-xl dark:text-gray-50">Education</p>
@@ -27,7 +27,7 @@ function Resume() {
                     </div>
                     ))}
                 </div>
-                <div className="w-full md:w-1/2">
+                <div className="w-full lg:w-1/2">
                     <div className="flex items-start mb-7 mt-7 md:mt-0">
                         <Briefcase size={30} color="#0891b2" />
                         <p className="ml-1 mt-1 text-xl dark:text-gray-50">Experience</p>
