74a6375093f46a086ef3bfe6fdcc2e31ab93bdac
fiimaarif <fiimaarif44@gmail.com>
2025-05-18 21:16:29 +0700

    [UPDATE] - adjust padding in Knowledges section for improved layout consistency

--- src/components/main/resume/index.tsx
+++ src/components/main/resume/index.tsx
@@ -48,9 +48,9 @@ function Resume() {
                 </div>
             </div>
             <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50 my-5">Knowledges</h1>
-            <div className="flex gap-1 md:gap-1 lg:gap-x-4 lg:gap-y-3 lg:flex-wrap justify-between lg:justify-start overflow-x-auto">
+            <div className="flex gap-1 md:gap-1 lg:gap-x-4 lg:gap-y-3 lg:flex-wrap justify-between lg:justify-start overflow-x-auto py-2 lg:py-0">
                 {knowledges.map((item, index) => (
-                    <div key={index} className="bg-red-400 dark:bg-slate-700 px-5 py-1 rounded-3xl font-medium text-gray-50 min-w-max">{item.label}</div>
+                    <div key={index} className="bg-red-400 dark:bg-slate-700 px-5 py-2 lg:py-1 rounded-3xl font-medium text-gray-50 min-w-max">{item.label}</div>
                 ))}
             </div>
         </main>
