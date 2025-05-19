45ffe86918bc1726e3c58afac3874232e784209a
fiimaarif <fiimaarif44@gmail.com>
2025-05-19 21:15:26 +0700

    [UPDATE] - adjust margin of Contact heading for improved layout consistency

--- src/components/main/contact/index.tsx
+++ src/components/main/contact/index.tsx
@@ -6,9 +6,10 @@ function Contact() {
     const formspreeId = process.env.NEXT_PUBLIC_FORMSPREE_ID;
     return (
         <main className="p-8 bg-slate-50 dark:bg-slate-800 rounded-3xl mt-2 md:mt-0 mb-12 md:mb-0">
-            <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 dark:text-gray-50 mb-5">
+            <h1 className="text-2xl xl:text-3xl font-semibold text-gray-800 dark:text-gray-50 mb-2">
                 Contact
             </h1>
+            <hr className="border-dashed border-2 border-gray-300 dark:border-gray-600 mb-5" />
             <article className="py-5 w-full md:w-full xl:w-8/12">
                 <p className="text-base md:text-lg text-neutral-600 dark:text-gray-50 text-justify md:text-left">
                     I'm excited to connect with you! Whether you have questions,
