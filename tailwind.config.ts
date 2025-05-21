73bbfb07427800548a2009af115206ff190b1d3c
fiimaarif <fiimaarif44@gmail.com>
2025-05-21 20:30:33 +0700

    [UPDATE] - format tailwind.config.ts for improved readability and consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-      },
+    darkMode: "class",
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+            },
+
+            keyframes: {
+                slide: {
+                    "0%": { transform: "translateX(0)" },
+                    "100%": { transform: "translateX(-50%)" },
+                },
+            },
+            animation: {
+                slide: "slide 20s linear infinite",
+            },
+        },
     },
-  },
-  plugins: [require('tailwind-scrollbar-hide')],
+    plugins: [require("tailwind-scrollbar-hide")],
 };
 export default config;
