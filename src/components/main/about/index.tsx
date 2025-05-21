9361acd0aa94fa2e342c41506fe16b82df474012
fiimaarif <fiimaarif44@gmail.com>
2025-05-21 20:30:40 +0700

    [UPDATE] - refactor About component for improved readability and structure

--- src/components/main/about/index.tsx
+++ src/components/main/about/index.tsx
@@ -32,43 +32,36 @@ function About() {
                 <div className="grid lg:grid-cols-12 gap-5">
                     <div className="bg-red-50 dark:bg-slate-700 p-3 col-span-12 md:col-span-6 rounded-xl">
                         <div className="bg-emerald-100 dark:bg-slate-500 w-min p-2 rounded-xl mb-2">
-                            <Layout
-                                size={30}
-                                color="#d946ef"
-                            />
+                            <Layout size={30} color="#d946ef" />
                         </div>
-                            <div>
-                                <p className="text-xl text-neutral-800 dark:text-gray-50 font-semibold">
-                                    Web Development
-                                </p>
-                                <hr className="my-1"/>
-                                <p className="text-neutral-600 dark:text-gray-50 text-left">
-                                    I develop modern, responsive websites using
-                                    the latest technologies to ensure optimal
-                                    performance and an outstanding user
-                                    experience.
-                                </p>
-                            </div>
+                        <div>
+                            <p className="text-xl text-neutral-800 dark:text-gray-50 font-semibold">
+                                Web Development
+                            </p>
+                            <hr className="my-1" />
+                            <p className="text-neutral-600 dark:text-gray-50 text-left">
+                                I develop modern, responsive websites using the
+                                latest technologies to ensure optimal
+                                performance and an outstanding user experience.
+                            </p>
+                        </div>
                     </div>
                     <div className="bg-emerald-50 dark:bg-slate-700 p-3 col-span-12 md:col-span-6 rounded-xl">
-                            <div className="bg-red-100 dark:bg-slate-500 w-min p-2 rounded-xl mb-2">
-                                <Codesandbox
-                                    size={30}
-                                    color="#fb7185"
-                                />
-                            </div>
-                            <div>
-                                <p className="text-xl text-neutral-800 dark:text-gray-50 font-semibold">
-                                    Mobile Development
-                                </p>
-                                <hr className="my-1"/>
-                                <p className="text-neutral-600 dark:text-gray-50 text-left">
-                                    I create high-performance mobile
-                                    applications tailored to your needs,
-                                    ensuring seamless functionality and an
-                                    engaging user experience across all devices.
-                                </p>
-                            </div>
+                        <div className="bg-red-100 dark:bg-slate-500 w-min p-2 rounded-xl mb-2">
+                            <Codesandbox size={30} color="#fb7185" />
+                        </div>
+                        <div>
+                            <p className="text-xl text-neutral-800 dark:text-gray-50 font-semibold">
+                                Mobile Development
+                            </p>
+                            <hr className="my-1" />
+                            <p className="text-neutral-600 dark:text-gray-50 text-left">
+                                I create high-performance mobile applications
+                                tailored to your needs, ensuring seamless
+                                functionality and an engaging user experience
+                                across all devices.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -77,20 +70,28 @@ function About() {
                     Languages and Tools
                 </h1>
                 <div>
-                    <div className="flex justify-center gap-5 mt-10 bg-gray-100 dark:bg-slate-700 p-7 rounded-2xl overflow-x-auto scrollbar-hide">
-                        {tech.map((item, index) => (
-                            <div className="flex items-center bg-white dark:bg-slate-600 shadow-md rounded-full py-1 px-5 gap-1 min-w-max">
-                                <img
-                                    key={index}
-                                    title={item.alt}
-                                    width={22}
-                                    height={22}
-                                    src={item.image}
-                                    alt={item.alt}
-                                />
-                                <p className="text-black dark:text-white">{item.name}</p>
+                    <div className="flex justify-center gap-5 mt-10 bg-gray-100 dark:bg-slate-700 rounded-2xl overflow-x-auto scrollbar-hide">
+                        <div className="relative overflow-hidden px-0 py-7">
+                            <div className="absolute left-0 top-0 h-full w-16 bg-gradient-to-r from-white dark:from-slate-900 to-transparent z-10 pointer-events-none" />
+                            <div className="absolute right-0 top-0 h-full w-16 bg-gradient-to-l from-white dark:from-slate-900 to-transparent z-10 pointer-events-none" />
+                            <div className="animate-slide flex gap-4 w-max">
+                                {tech.map((item, index) => (
+                                    <div className="flex items-center bg-white dark:bg-slate-600 shadow-md rounded-full py-1 px-5 gap-1 min-w-max">
+                                        <img
+                                            key={index}
+                                            title={item.alt}
+                                            width={22}
+                                            height={22}
+                                            src={item.image}
+                                            alt={item.alt}
+                                        />
+                                        <p className="text-black dark:text-white">
+                                            {item.name}
+                                        </p>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                        </div>
                     </div>
                 </div>
             </div>
